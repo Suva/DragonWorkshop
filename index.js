59e7e79ba79dd2aed79cad66a922e419ae54f6bd
Jaan Pullerits <clysuva@gmail.com>
2018-04-20 00:55:51 +0300

    Make nicer :)

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const name = '';
-const service = require('esticade')(name)
+const service = require('esticade')(name);
 
-service.on('KnightSpotted', ev => {
-    const {gameId, knight} = ev.body
-    // service.emit("DragonAppears", {gameId, dragon})
-})
+service.on('Knight Spotted', ev => {
+    const {gameId, knight} = ev.body;
+    // service.emit("Dragon Appears", {gameId, dragon});
+});
 
-service.on('WeatherReport', ev => {
-    const {gameId, code} = ev.body
-})
-
-service.on('Victory', ev => { console.log("Victory", ev.body.player) })
-service.on('Defeat', ev => { console.log("Defeat", ev.body.player) })
+/**************
+ * Other events:
+ * - Weather Report
+ * - Victory
+ * - Defeat
+ * - Already Defeated
+ */
 
