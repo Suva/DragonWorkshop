fd7b33df10f95635f4e646a41507ab82f24346e5
Jaan Pullerits <clysuva@gmail.com>
2018-04-20 09:14:11 +0300

    Add prewritten code

---
+++ dbRecorder.js
@@ -0,0 +1,16 @@
+const db = require('./db')('Jaan Pullerits', '')
+const esticade = require('esticade')('Jaan Pullerits Event Recorder')
+
+async function run() {
+    let events = await db.collection('events')
+
+    esticade.on('Victory', recordEvent)
+    esticade.on('Defeat', recordEvent)
+
+    function recordEvent(event) {
+        console.log('Recoding event', event.name, event.body.player)
+        events.insert(event)
+    }
+}
+
+run()
---
+++ index2.js
@@ -0,0 +1,53 @@
+const name = 'Mister Dark';
+
+//////////////////////////////////////////
+
+const service = require('esticade')(name)
+// const solver = require('mugloar-solver')
+
+const games = {}
+
+function addGamePiece(gameId, piece) {
+    let game = games[gameId]
+
+    if (!game) {
+        game = {}
+    }
+
+    game = {...game, ...piece}
+    games[gameId] = game
+
+    if (game.knight && game.weather) {
+        console.log("We have game data!")
+        // let dragon = solver.solve(game.knight, game.weather);
+        service.emit("Dragon Appears", {gameId, dragon: null})
+    }
+}
+
+service.on('Knight Spotted', ev => {
+    try {
+        const {gameId, knight} = ev.body
+        console.log({gameId, knight: knight.name})
+        addGamePiece(gameId, {knight})
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+service.on('Weather Report', ev => {
+    try {
+        const {gameId, code: weather} = ev.body
+        console.log({gameId, weather})
+        addGamePiece(gameId, {weather})
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+service.on('Victory', (ev) => {
+    console.log("Victory", ev.body.player)
+})
+
+service.on('Defeat', (ev) => {
+    console.log("Defeat", ev.body.player)
+})
